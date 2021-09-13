f1718742d1442af797107efde59e3bc430a67d25
dane <dane.m.petersen@gmail.com>
2021-09-13 10:07:08 -0500

    app js code cleanup

--- src/App.js
+++ src/App.js
@@ -28,14 +28,12 @@ import AboutPage from './components/AboutPage/AboutPage'
 Amplify.configure(awsExports);
 
 function App() {
-  const [todos, setTodos] = useState([])
   const [lat, setLat] = useState('');
   const [lng, setLng] = useState('');
 
   useEffect(() => {
     fetuserLocation()
   }, [])
-
 
   /**
    * Fetch User Location
@@ -57,198 +55,62 @@ function App() {
     <Router>
       <div className="main">
         <Nav />
-        <Switch>
-          {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-          <Redirect exact from="/" to="/home" />
+        <div className="container">
+          <Switch>
+            {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+            <Redirect exact from="/" to="/home" />
 
-          {/* Visiting localhost:3000/about will show the about page. */}
-          {/* <Route
-            // shows AboutPage at all times (logged in or not)
-            exact
-            path="/hike"
-          >
-            <Hike />
-          </Route> */}
+            <Route
+              // shows AboutPage at all times (logged in or not)
+              exact
+              path="/hike"
+            >
+              <Hike
+                latLng = {[lat, lng]}
+              />
+            </Route>
+            <Route
+              // shows AboutPage at all times (logged in or not)
+              exact
+              path="/hikes"
+            >
+              <Hikes/>
+            </Route>
 
-          <Route
-            // shows AboutPage at all times (logged in or not)
-            exact
-            path="/hike"
-          >
-            <Hike
-              latLng = {[lat, lng]}
-            />
-          </Route>
-          <Route
-            // shows AboutPage at all times (logged in or not)
-            exact
-            path="/hikes"
-          >
-            <Hikes/>
-          </Route>
+            <Route
+              // shows AboutPage at all times (logged in or not)
+              exact
+              path="/about"
+            >
+              <AboutPage />
+            </Route>
 
-          <Route
-            // shows AboutPage at all times (logged in or not)
-            exact
-            path="/about"
-          >
-            <AboutPage />
-          </Route>
+            <Route
+              // shows AboutPage at all times (logged in or not)
+              exact
+              path="/add"
+            >
+              <AddHike 
+                latLng = {[lat, lng]}
+              />
+            </Route>
 
-          <Route
-            // shows AboutPage at all times (logged in or not)
-            exact
-            path="/add"
-          >
-            <AddHike 
-              latLng = {[lat, lng]}
-            />
-          </Route>
+            <Route
+              exact
+              path="/home"
+            >
+              <LandingPage /> 
+            </Route>
 
-          {/* For protected routes, the view could show one of several things on the same route.
-            Visiting localhost:3000/user will show the UserPage if the user is logged in.
-            If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
-            Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-          {/* <ProtectedRoute
-            // logged in shows UserPage else shows LoginPage
-            exact
-            path="/user"
-          >
-            <UserPage />
-          </ProtectedRoute> */}
-
-          {/* <ProtectedRoute
-            // logged in shows InfoPage else shows LoginPage
-            exact
-            path="/info"
-          >
-            <InfoPage />
-          </ProtectedRoute> */}
-
-          {/* <Route
-            exact
-            path="/login"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect to the /user page
-              <Redirect to="/user" />
-              :
-              // Otherwise, show the login page
-              <LoginPage />
-            }
-          </Route> */}
-
-          {/* <Route
-            exact
-            path="/registration"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect them to the /user page
-              <Redirect to="/user" />
-              :
-              // Otherwise, show the registration page
-              <RegisterPage />
-            }
-          </Route> */}
-
-          <Route
-            exact
-            path="/home"
-          >
-            <LandingPage /> 
-          </Route>
-
-          {/* If none of the other routes matched, we will show a 404. */}
-          <Route>
-            <h1>404</h1>
-          </Route>
-        </Switch>
-        {/* <Footer /> */}
+            {/* If none of the other routes matched, we will show a 404. */}
+            <Route>
+              <h1>404</h1>
+            </Route>
+          </Switch>
+          </div>
       </div>
     </Router>
-    // <Router>
-    //   <div className="main">
-    //     <Switch>
-    //       {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-    //       <Redirect exact from="/" to="/home" />
-    //       {
-    //         <Route>
-    //           <Hike/>
-    //         </Route>
-    //                     <Route>
-    //                     <Map />
-    //                   </Route>
-
-
-    //         // <Route
-    //         // exact
-    //         // path="/home"
-    //         // >
-    //         // {user.id ?
-    //         //   // If the user is already logged in, 
-    //         //   // redirect them to the /user page
-    //         //   <Redirect to="/user" />
-    //         //   :
-    //         //   // Otherwise, show the Landing page
-    //         //   <LandingPage />
-    //         // }
-    //         // </Route>
-
-    //       }
-    //     </Switch>
-    //   </div>
-    // </Router>
-
-
-  // <div style={styles.container}>
-  //     <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-  //       <TileLayer
-  //         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-  //         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  //       />
-  //       <Marker position={[51.505, -0.09]}>
-  //         <Popup>
-  //         A pretty CSS3 popup. <br /> Easily customizable.
-  //         </Popup>
-  //       </Marker>
-  //     </MapContainer>
-  //     <h2>Amplify Todos</h2>
-  //     <input
-  //       onChange={event => setInput('name', event.target.value)}
-  //       style={styles.input}
-  //       value={formState.name}
-  //       placeholder="Name"
-  //     />
-  //     <input
-  //       onChange={event => setInput('description', event.target.value)}
-  //       style={styles.input}
-  //       value={formState.description}
-  //       placeholder="Description"
-  //     />
-  //     <button style={styles.button} onClick={addTodo}>Create Todo</button>
-      
-  //     {
-  //       todos.map((todo, index) => (
-  //         <div key={todo.id ? todo.id : index} style={styles.todo}>
-  //           <p style={styles.todoName}>{todo.name}</p>
-  //           <p style={styles.todoDescription}>{todo.description}</p>
-  //         </div>
-  //       ))
-  //     }
-  //     <AmplifySignOut />
-  //   </div>
   );
-}
-
-const styles = {
-  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  todo: {  marginBottom: 15 },
-  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
-  todoName: { fontSize: 20, fontWeight: 'bold' },
-  todoDescription: { marginBottom: 0 },
-  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
 
 export default App
--- src/components/Hikes/Hikes.jsx
+++ src/components/Hikes/Hikes.jsx
@@ -52,9 +52,9 @@ function Hikes() {
       {/* TODO MAP */}
       {
         hikes.map((todo, index) => (
-        <div key={todo.id ? todo.id : index} style={styles.todo}>
-        <p style={styles.todoName}>{todo.name}</p>
-        <p style={styles.todoDescription}>{todo.description}</p>
+        <div key={todo.id ? todo.id : index} className="card hike-card">
+          <p>{todo.name}</p>
+          <p>{todo.description}</p>
         </div>
         ))
       }
@@ -63,14 +63,6 @@ function Hikes() {
   );
 }
 
-const styles = {
-  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  todo: {  marginBottom: 15 },
-  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
-  todoName: { fontSize: 20, fontWeight: 'bold' },
-  todoDescription: { marginBottom: 0 },
-  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
-}
 
 
 export default withAuthenticator(Hikes)
