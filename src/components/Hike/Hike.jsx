1281fcd9e702310da622989f9decaac65ac878e0
dane <dane.m.petersen@gmail.com>
2021-09-13 22:26:07 -0500

    moved map from map component to hike component

--- src/components/Hike/Hike.jsx
+++ src/components/Hike/Hike.jsx
@@ -4,7 +4,7 @@ import './Hike.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import { Auth } from 'aws-amplify';
 import { useSelector } from 'react-redux';
-
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 /**
  * Hike Component
  * App.js retrieves user location and passes the lat/long via props to the map component
@@ -14,7 +14,7 @@ function Hike() {
   const user = useSelector((store) => store.user);
   const hike = useSelector(store => store.hikeReducer);
   // console.log('hike', hike.mapdata);
-  console.log('hike parse', JSON.parse(hike.mapdata));
+  // console.log('hike parse', JSON.parse(hike.mapdata));
   async function checkAuthState() {
     try {
       await Auth.currentAuthenticatedUser()
@@ -30,9 +30,23 @@ function Hike() {
   return (
     <div className="hike-map-container">
       <h1>Hike</h1>
-      {/* <Map
-        // latLng = {latLng.latLng}
-      /> */}
+      <MapContainer zoom={16} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <TileLayer
+        url={`https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=11ec4ec7b29812e54c0f261032fbce7b`}
+      />
+      <TileLayer
+        url={`https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=11ec4ec7b29812e54c0f261032fbce7b`}
+      />
+      {/* <Marker position={latLng.latLng}>
+        <Popup>
+          FOUND YOU! 
+        </Popup>
+      </Marker> */}
+    </MapContainer>
     </div>
   );
 }
